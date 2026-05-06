8300c6b3c6055d9a2cfd0f1e5cfd15be611b0cd3
abdulrahmanalzagam <abdulrahmanalzagam@gmail.com>
2026-05-07 01:48:36 +0300

    add update to genral template

--- library_project/urls.py
+++ library_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 from books import views
 
 urlpatterns = [
@@ -13,3 +15,6 @@ urlpatterns = [
     # هذا هو الحل
     path('', RedirectView.as_view(url='/books/', permanent=False)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
